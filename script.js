84337307ecf9b9e58ab46372dc1eae3c7d1897f0
Irfan Shadik <77362427+irfanshadikofficial@users.noreply.github.com>
2022-07-18 02:03:11 +0600

    v2.0

--- script.js
+++ script.js
@@ -3,4 +3,19 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("light-mode");
  }
- //dark-mode
+//dark-mode
+
+
+//toogle-image-of-the-mood
+const img = document.getElementById('change_sun');
+let toggle = true;
+img.addEventListener('click', function(){
+    toggle = !toggle;
+    if(toggle){
+        img.src= 'img\\sun.png';
+    }
+    else{
+        img.src= 'img\\moon.png'
+    }
+})
+//toogle-image-of-the-mood
